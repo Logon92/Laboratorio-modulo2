32a54f3a4d9a2dfc70d632d73ad45d6a26443fde
Logon92 <you@example.com>
2024-02-26 20:30:36 +0100

    guardar cambios

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
 import "./style.css";
 
-console.log("Hello from main");
+console.log("Ticket de cena: 120€");
+console.log("Bebidas: 18€")
+console.log("Ticket de cena - bebidas / 6 amigos")
+console.log((120-18)/6)
