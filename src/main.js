445ba302f951f15219139da63f72d9ce37e60116
Logon92 <you@example.com>
2024-02-28 11:27:43 +0100

    commit final

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "./style.css";
 
-console.log("Ticket de cena: 120€");
-console.log("Bebidas: 18€")
-console.log("Ticket de cena - bebidas / 6 amigos")
+//Ticket de cena = 120€
+//Bebidas = 18€
+//Ticket de cena - bebidas / 6 amigos
+
 console.log((120-18)/6)
